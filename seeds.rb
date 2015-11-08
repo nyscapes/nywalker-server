745d0ec2a2a5bc5c56d7330061eb8a59356f750b
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-11-07 23:37:34 -0500

    add initial seed to make an admin user (me)

---
+++ seeds.rb
@@ -0,0 +1,7 @@
+require_relative "./model"
+
+# Set these for something useful
+root = User.create(username: "moacir", password: "1234", email: "moacir@blah.com", name: "Moacir", admin: true, added_on: Time.now)
+
+
+
