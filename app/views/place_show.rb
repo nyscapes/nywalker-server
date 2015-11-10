aadf83bf5b4194e8d9d367ac7f8d8bfc215758e7
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-11-09 20:31:35 -0500

    Get the place route up and running with a map. Also standardize big maps via css.

--- app/views/place_show.rb
+++ app/views/place_show.rb
@@ -15,6 +15,29 @@ class App
         @place.added_on
       end
 
+      def lat
+        @place.lat
+      end
+
+      def lon
+        @place.lon
+      end
+
+      def bounding_box_polygon
+        if @place.bounding_box_string =~ /^\[.*\]$/
+          string = "var boundingBox = L.polygon(["
+          bbox = @place.bounding_box_string.gsub(/^\[/, "").gsub(/\]$/, "").split(", ").map{ |n| n.to_f }
+          string = string + "[#{bbox[1]}, #{bbox[0]}],"
+          string = string + "[#{bbox[1]}, #{bbox[3]}],"
+          string = string + "[#{bbox[2]}, #{bbox[3]}],"
+          string = string + "[#{bbox[2]}, #{bbox[0]}]"
+          string = string + "], {color: '#fff', opacity: 0.8}).addTo(map);\n"
+          string = string + "map.fitBounds([[#{bbox[1]}, #{bbox[3]}], [#{bbox[2]}, #{bbox[0]}]]);\n"
+        else
+          ""
+        end
+      end
+
     end
   end
 end
