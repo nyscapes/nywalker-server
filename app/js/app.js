43e530f88e011351af3b0208f21f5581f4323ab4
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-11-27 18:20:48 -0500

    Move function out of app.js to the only place it's used.

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,3 @@
-function fillInPlaceData(data){
-  // `data` is a string that looks like "name##lat##lon##geonameid##bbox##source##confidence"
-  var array = data.split("##");
-  $.each(['#inputName', '#inputLat', '#inputLon', '#inputGeonameid', '#inputBbox', '#inputSource', '#inputConfidence'], function( index, value ){
-    $(value).val(
-      array[index]
-    );
-  });
-  what3words.positionToWords([array[1], array[2]], function(words){
-    var dotWords = words.join(".");
-    $('#inputW3W').val(dotWords);
-  });
-}
 
 function recenterMap(latLng){
   marker.setLatLng(latLng);
