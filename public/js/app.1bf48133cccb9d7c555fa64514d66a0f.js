aace153c501c25eccb6ea72f96ccfe5f070dcbc5
Moacir P. de Sá Pereira <moacir@gmail.com>
2015-11-28 19:38:17 -0500

    another effort at assets

--- public/js/app.1bf48133cccb9d7c555fa64514d66a0f.js
+++ public/js/app.1bf48133cccb9d7c555fa64514d66a0f.js
@@ -1,12 +1,3 @@
-function fillInPlaceData(data){
-  // `data` is a string that looks like "name##lat##lon##geonameid##bbox##source##confidence"
-  var array = data.split("##");
-  $.each(['#inputName', '#inputLat', '#inputLon', '#inputGeonameid', '#inputBbox', '#inputSource', '#inputConfidence'], function( index, value ){
-    $(value).val(
-      array[index]
-    );
-  });
-}
 
 function recenterMap(latLng){
   marker.setLatLng(latLng);
