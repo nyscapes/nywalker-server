51f1adcca1dac131cb68a8b1ce5ee43cd30c63ae
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-01-21 13:46:28 -0500

    add flag and note icons to instance list in book_show.

--- app/views/book_show.rb
+++ app/views/book_show.rb
@@ -45,9 +45,13 @@ class App
       end
 
       def instances
-        @instances.map{ |i| {page: i.page, sequence: i.sequence, place_name: i.place.name, place_slug: i.place.slug, instance_id: i.id,
+        @instances.map{ |i| {
+          page: i.page, sequence: i.sequence, place_name: i.place.name, place_slug: i.place.slug, instance_id: i.id,
           instance_permitted: ( admin? || i.user == @user ),
-          owner: i.user.name } }
+          owner: i.user.name,
+          flagged: i.flagged,
+          note: not_empty(i.note)
+        } }
       end
 
     end
