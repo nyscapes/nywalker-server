f91d85b0eaac2d9291ab83746904770783838b85
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-02-15 00:16:21 -0500

    Add postgis script that changes a regular db into a postGIS db.

---
+++ postgis.rb
@@ -0,0 +1,9 @@
+require './app'
+Place.each do |p|
+  if p.geom.nil?
+    unless p.lat.nil? || p.lon.nil?
+      p.geom = GeoRuby::SimpleFeatures::Point.from_x_y(p.lon, p.lat, 4326)
+      p.save
+    end
+  end
+end
