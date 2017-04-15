35e3ec56c082775bb541e6c0ca452da060529855
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-04-15 01:15:51 +0000

    Oops. Saving a place doesn't count as an instance…

--- app/routes/place.rb
+++ app/routes/place.rb
@@ -45,7 +45,7 @@ class App
     end
     begin
       new_place.save
-      Nickname.create(name: new_place.name, place: new_place, instance_count: 1)
+      Nickname.create(name: new_place.name, place: new_place, instance_count: 0)
       if params[:form_source] == "modal"
         @place = new_place
         mustache :modal_place_saved, { layout: :naked }
