60e14270afbc60be5fd7716412f37ab847171ef9
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-11 17:55:06 -0400

    Keep adding tests.

--- tests/acceptance/list-places-test.js
+++ tests/acceptance/list-places-test.js
@@ -13,5 +13,17 @@ test('visiting /places', function(assert) {
 
 test('should show a list of places', function(assert){});
 
+test('should filter the list of places', function(assert){
+  visit('/places');
+  fillIn('.place-filter input', 'Moscow');
+  keyEvent('.place-filter input', 'keyup', 69);
+
+  andThen(function() {
+    assert.equal(find('.place').length, 1, 'should show 1 place');
+    assert.equal(find('.place .location:contains("Moscow")').length, 1, 'should contain 1 listing with location Moscow');
+  });
+});
+
+
 test('should show a map with all the places', function(assert){});
 
--- tests/integration/components/place-filter-test.js
+++ tests/integration/components/place-filter-test.js
@@ -1,25 +1,61 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+import RSVP from 'rsvp';
+
+const ITEMS = [{name: 'New York City'}, {name: 'New York'}, {name: 'Moscow'}];
+const RESULTS = [{name: 'Moscow'}];
 
 moduleForComponent('place-filter', 'Integration | Component | place filter', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it should initially load all places', function(assert) {
+  // We return promises
+  this.on('filterByName', (val) => {
+    if (val === ''){
+      return RSVP.resolve(ITEMS);
+    } else {
+      return RSVP.resolve(RESULTS);
+    }
+  });
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{place-filter}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+	// with an integration test,
+  // you can set up and use your component in the same way your application will use it.
   this.render(hbs`
-    {{#place-filter}}
-      template block text
+    {{#place-filter filter=(action 'filterByName') as |results|}}
+      <ul>
+      {{#each results as |item|}}
+        <li class="place">
+          {{item.name}}
+        </li>
+      {{/each}}
+      </ul>
     {{/place-filter}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  return wait().then(() => {
+    assert.equal(this.$('.place').length, 3);
+    assert.equal(this.$('.place').first().text().trim(), 'New York City');
+  });
+
 });
+
+// test('it renders', function(assert) {
+
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
+
+//   this.render(hbs`{{place-filter}}`);
+
+//   assert.equal(this.$().text().trim(), '');
+
+//   // Template block usage:
+//   this.render(hbs`
+//     {{#place-filter}}
+//       template block text
+//     {{/place-filter}}
+//   `);
+
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
