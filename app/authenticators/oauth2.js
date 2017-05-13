842f4d5e8df08790e3d5c0216c078fa8bbf8eb40
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-12 23:50:12 -0400

    Get ember-simple-auth sending json to the api server.

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,39 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenRevocationEndpoint: '/revoke'
+  serverTokenEndpoint: '/api/v1/token',
+  serverTokenRevocationEndpoint: '/api/v1/revoke',
+  makeRequest: function(url, data, headers = {}) {
+    headers['Content-Type'] = 'application/vnd.api+json';
+    headers['Accept'] = 'application/vnd.api+json';
+    console.log(headers);
+    return this._super(url, data, headers);
+    // const body = keys(data).map((key) => {
+    //   return `${key}=${data[key]}`;
+    // }).join('&');
+
+    // const options = {
+    //   body,
+    //   headers,
+    //   method: 'POST'
+    // };
+
+    // const clientIdHeader = this.get('_clientIdHeader');
+    // if (!isEmpty(clientIdHeader)) {
+    //   merge(options.headers, clientIdHeader);
+    // }
+    // return new RSVP.Promise((resolve, reject) => {
+    //   fetch(url, options).then((response) => {
+    //     response.text().then((text) => {
+    //       let json = text ? JSON.parse(text) : {};
+    //       if (!response.ok) {
+    //         response.responseJSON = json;
+    //         reject(response);
+    //       } else {
+    //         resolve(json);
+    //       }
+    //     });
+    //   }).catch(reject);
+    // });
+  }
 });
