ba1b70972716c0db5bb94c16a73021a05f6cc06d
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-13 01:10:22 -0400

    Send valid data to API server

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,4 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import RSVP from 'rsvp';
 
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: '/api/v1/token',
@@ -6,33 +7,26 @@ export default OAuth2PasswordGrant.extend({
   makeRequest: function(url, data, headers = {}) {
     headers['Content-Type'] = 'application/vnd.api+json';
     headers['Accept'] = 'application/vnd.api+json';
-    return this._super(url, data, headers);
-    // const body = keys(data).map((key) => {
-    //   return `${key}=${data[key]}`;
-    // }).join('&');
+    const body = JSON.stringify(data);
 
-    // const options = {
-    //   body,
-    //   headers,
-    //   method: 'POST'
-    // };
+    const options = {
+      body,
+      headers,
+      method: 'POST'
+    };
 
-    // const clientIdHeader = this.get('_clientIdHeader');
-    // if (!isEmpty(clientIdHeader)) {
-    //   merge(options.headers, clientIdHeader);
-    // }
-    // return new RSVP.Promise((resolve, reject) => {
-    //   fetch(url, options).then((response) => {
-    //     response.text().then((text) => {
-    //       let json = text ? JSON.parse(text) : {};
-    //       if (!response.ok) {
-    //         response.responseJSON = json;
-    //         reject(response);
-    //       } else {
-    //         resolve(json);
-    //       }
-    //     });
-    //   }).catch(reject);
-    // });
+    return new RSVP.Promise((resolve, reject) => {
+      fetch(url, options).then((response) => {
+        response.text().then((text) => {
+          let json = text ? JSON.parse(text) : {};
+          if (!response.ok) {
+            response.responseJSON = json;
+            reject(response);
+          } else {
+            resolve(json);
+          }
+        });
+      }).catch(reject);
+    });
   }
 });
