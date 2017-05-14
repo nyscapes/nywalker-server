e5fe666437c35b6fae7b648a71633f5f9da3ec39
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-14 01:22:38 -0400

    Hide the infinity loader when it's searching for places

--- app/controllers/places.js
+++ app/controllers/places.js
@@ -4,8 +4,10 @@ export default Ember.Controller.extend({
   actions: {
     filterByName(param) {
       if (param.length > 2) {
+        $('.infinity-loader').hide();
         return this.get('store').query('place', { name: param });
       } else {
+        $('.infinity-loader').show();
         return this.get('store').findAll('place');
       }
     }
