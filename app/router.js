63165a8632071cbb2d378b9c177b6aaff73f9cc8
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-14 16:09:16 -0400

    Get books.show up and running.

--- app/router.js
+++ app/router.js
@@ -14,7 +14,9 @@ Router.map(function() {
   });
   this.route('login');
   this.route('places-pages');
-  this.route('books');
+  this.route('books', function() {
+    this.route('show', { path: '/:book_slug' });
+  });
 });
 
 export default Router;
--- apprb/routes/api.rb
+++ apprb/routes/api.rb
@@ -67,7 +67,13 @@ class App
 
   # Books
     get '/books' do
-      serialize_models(Book.all.sort_by{ |b| b.instance_count }.reverse).to_json
+      if params[:slug]
+        book = Book.where(slug: params[:slug]).first
+        not_found if book.nil?
+        serialize_model(book, include: 'instances').to_json
+      else
+        serialize_models(Book.all.sort_by{ |b| b.instance_count }.reverse).to_json
+      end
     end
 
   # Instances
