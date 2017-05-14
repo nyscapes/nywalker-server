3a20be4ba80a2b2cced745a4a84d6df6a579e92b
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-14 01:09:59 -0400

    only start filtering places when there are more than two letters

--- app/controllers/places.js
+++ app/controllers/places.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     filterByName(param) {
-      if (param !== '') {
+      if (param.length > 2) {
         return this.get('store').query('place', { name: param });
       } else {
         return this.get('store').findAll('place');
