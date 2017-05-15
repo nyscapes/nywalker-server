315b87c23f5a1443b669752f6106c2b1129b4c43
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-15 00:57:04 -0400

    Create a books/text that loads on regular books route but is replaced.

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate() {
+    this.render();
+    this.render('books/text', {
+      into: 'books',
+      outlet: 'books-show'
+    });
+  },
   model() {
     return this.get('store').findAll('book');
   }
--- app/routes/books/show.js
+++ app/routes/books/show.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate() {
+    this.render({
+      into: 'books',
+      outlet: 'books-show'
+    });
+  },
   model(params) {
     return this.get('store').queryRecord('book', { slug: params.book_slug });
   }
