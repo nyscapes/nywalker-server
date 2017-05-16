a170368e5b3da237d6ee070bc6a9bccf7a4f8ced
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-16 18:09:52 -0400

    Hide infinity loader.

--- app/controllers/places.js
+++ app/controllers/places.js
@@ -4,10 +4,10 @@ export default Ember.Controller.extend({
   actions: {
     filterByName(param) {
       if (param.length > 2) {
-        $('.infinity-loader').hide();
+        // $('.infinity-loader').hide();
         return this.get('store').query('place', { name: param });
       } else {
-        $('.infinity-loader').show();
+        // $('.infinity-loader').show();
         return this.get('store').findAll('place');
       }
     }
--- app/routes/places.js
+++ app/routes/places.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
-import InfinityRoute from 'ember-infinity/mixins/route';
+// import InfinityRoute from 'ember-infinity/mixins/route';
 
+// export default Ember.Route.extend(InfinityRoute, {
+//   perPageParam: 'page_size',
+//   pageParam: 'data_page',
+//   model() {
+//     return this.infinityModel('place', { perPage: 10, startingPage: 1 });
+//   }
+// });
 
-export default Ember.Route.extend(InfinityRoute, {
-  perPageParam: 'page_size',
-  pageParam: 'data_page',
+export default Ember.Route.extend({
   model() {
-    return this.infinityModel('place', { perPage: 10, startingPage: 1 });
+    return this.get('store').findAll('place');
   }
 });
