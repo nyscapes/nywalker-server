b1e117288415aca911eb482b14b30bc5a4a30de5
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-16 20:40:06 -0400

    Get book map working w/ mappable instances and nicknames.

--- app/controllers/books/show.js
+++ app/controllers/books/show.js
@@ -2,10 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortKey: ['page', 'sequence'],
-  sortedInstances:Ember.computed.sort('model.instances', 'sortKey'),
-  instancePlaces: Ember.computed.map('model.instances', function(instance, index) {
-    if (instance.mappable) {
-      return instance.latLng;
-    }
+  sortedInstances: Ember.computed.sort('model.instances', 'sortKey'),
+  isMappable: Ember.computed.filter('sortedInstances', function(instance) { return instance.get('mappable'); } ),
+  uniqLats: Ember.computed.uniqBy('isMappable', 'lat'),
+  uniqPlaces: Ember.computed.uniqBy('uniqLats', 'lon'),
+  //uniqPlaces: Ember.computed.uniqBy('isMappable', 'latLng'),
+  instancePlaces: Ember.computed('sortedInstances', 'uniqPlaces', function() {
+    let places = this.get('uniqPlaces');
+    let instances = this.get('sortedInstances');
+    let placesWithNicks = places.map(function(place) {
+      let instancesWithThisPlace = instances.filter(function(instance){
+        return instance.get('latLng') === place.get('latLng');
+      });
+      let nicknames = instancesWithThisPlace.map(function(instance){
+        return instance.get('text');
+      });
+      let nicknamesString = nicknames.join(', ');
+      place.set('nicknames', nicknamesString);
+      return place;
+    });
+    return placesWithNicks;
   })
 });
