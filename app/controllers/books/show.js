b49a86ce98ba373e1d17d597ba73887c9ef12507
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-16 22:39:18 -0400

    refactor the controller somewhat.

--- app/controllers/books/show.js
+++ app/controllers/books/show.js
@@ -2,33 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   bounds: Ember.computed('uniqPlaces', function() {
-    return L.latLngBounds(this.get('uniqPlaces').map(function(place) {
+    return L.latLngBounds(this.get('uniqPlaces').map(place => {
       return place.get('latLng');
     }));
   }),
   sortKey: ['page', 'sequence'],
   sortedInstances: Ember.computed.sort('model.instances', 'sortKey'),
-  isMappable: Ember.computed.filter('sortedInstances', function(instance) { return instance.get('mappable'); } ),
+  isMappable: Ember.computed.filter('sortedInstances', instance => { return instance.get('mappable'); } ),
   uniqLats: Ember.computed.uniqBy('isMappable', 'lat'),
   uniqPlaces: Ember.computed.uniqBy('uniqLats', 'lon'),
   // uniqPlaces: Ember.computed.uniqBy('isMappable', 'latLng'),
   instancePlaces: Ember.computed('sortedInstances', 'uniqPlaces', function() {
-    let places = this.get('uniqPlaces');
     let instances = this.get('sortedInstances');
-    let placesWithNicks = places.map(function(place) {
-      let instancesWithThisPlace = instances.filter(function(instance){
+    return this.get('uniqPlaces').map(place => {
+      let nicknames = instances.filter(instance => {
         return instance.belongsTo('place').id() === place.belongsTo('place').id();
-      });
-      console.log(instancesWithThisPlace);
-      let nicknames = instancesWithThisPlace.map(function(nickedInstance){
+      }).map(nickedInstance => {
         return nickedInstance.get('text');
       });
       let nicknamesString = nicknames.join(', ');
       place.set('nicknames', nicknamesString);
-      place.set('instancesInBook', instancesWithThisPlace.length);
-      // place.set('instancesInBook', nicknames.length);
+      place.set('instancesInBook', nicknames.length);
       return place;
     });
-    return placesWithNicks;
   })
 });
