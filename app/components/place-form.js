64015be5bedec2869d2b987b992a08f2877bd1fd
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-18 14:46:23 -0400

    Get a place form up and running.

--- app/components/place-form.js
+++ app/components/place-form.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model() { 
+    return this.get('store').createRecord('place');
+  },
   actions: {
-    submit() { alert('submit!'); }
+    addPlace() { alert('add place!'); },
+    search() { alert(this.get('search')); },
+    onChange(value, model, property) {
+      alert(`${value}, ${model}, ${property}`);
+    }
   }
 });
