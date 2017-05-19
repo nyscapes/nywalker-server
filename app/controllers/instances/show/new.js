c743cf8f4319eb26d4b925743d8105fb475bf040
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-05-19 00:42:28 -0400

    Get the promise for Place working on the ember side.

--- app/controllers/instances/show/new.js
+++ app/controllers/instances/show/new.js
@@ -1,15 +1,26 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Controller.extend({
   actions: {
     createInstance(instance) {
       let newInstance = this.get('store').createRecord('instance', instance);
-      let place = this.get('store').queryRecord('place', { slug: 'new-york-city' });
-      newInstance.set('place', place);
-      newInstance.set('lat', place.get('lat'));
-      newInstance.set('lon', place.get('lon'));
+      new RSVP.Promise((resolve, reject) => {
+        return this.get('store').queryRecord('place', { slug: 'new-york-city' }).then((place) => {
+          newInstance.set('place', place);
+          newInstance.set('lat', place.get('lat'));
+          newInstance.set('lon', place.get('lon'));
+          // newInstance.set('lat', place.lat);
+          // newInstance.set('lon', place.lon);
+          resolve();
+        }, reject);
+      });
+      // let place = this.get('store').queryRecord('place', { slug: 'new-york-city' });
+      // newInstance.set('place', place);
+      // newInstance.set('lat', place.get('lat'));
+      // newInstance.set('lon', place.get('lon'));
       newInstance.set('book', this.get('model'));
-      newInstance.set('user', this.get('store').findRecord('user', 1));
+      newInstance.set('user', this.get('loadCurrentUser')); //this.get('store').findRecord('user', 1));
       newInstance.save();
     }
   }
