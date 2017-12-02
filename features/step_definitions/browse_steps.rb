42600822aecf562eab906c41bab609ee2bf9a932
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-12-01 19:48:20 -0500

    Get basic books test frame up.

--- features/step_definitions/browse_steps.rb
+++ features/step_definitions/browse_steps.rb
@@ -2,8 +2,16 @@ Given(/^I am on the homepage$/) do
   visit '/'
 end
 
+Given(/^I am on the "([^"]*)" page$/) do |arg|
+  visit "/#{arg.downcase}"
+end
+
 Then(/^I should see the Title$/) do
   expect(page).to have_css "h1.display-3", text: "NYWalker"
+end
+
+Then(/^I should see an "([^"]*)" button$/) do |arg|
+  expect(page).to have_css "a.btn", text: arg
 end
 
 Then(/^I should see a map$/) do
