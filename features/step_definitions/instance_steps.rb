c3f1e75029011ca8e1614bc17e35c5334f74ab67
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-12-02 12:28:24 -0500

    Bring steps to order.

---
+++ features/step_definitions/instance_steps.rb
@@ -0,0 +1,37 @@
+When("I fill in instance information for {string}") do |string|
+  fill_in "Place", with: "#{string} -- {#{string}}"
+  fill_in "Place name in text", with: string
+  @instance_count = Instance.count
+end
+
+Then(/^the instance is saved$/) do
+  expect(Instance.count).to eq @instance_count + 1
+end
+
+When(/^I type "([^"]*)" in the "([^"]*)" field$/) do |arg1, arg2|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the form fills in "([^"]*)" as the nickname$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^fills in the full "([^"]*)" pattern$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I created one$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I see the last instance$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the new instance is on the same page as the previous$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the sequence is increased by one$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
--- features/step_definitions/researcher_steps.rb
+++
@@ -1,3 +0,0 @@
-Given(/^a researcher exists$/) do
-    pending # Write code here that turns the phrase above into concrete actions
-end
