8205ea747fccedf8dbea2e938f58633994fdbb75
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2017-12-03 03:20:32 -0500

    mark api tests are pending

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -4,11 +4,13 @@ describe "NYWalker API" do
   let(:apiurl) { "/api/v1" }
 
   it "should punt if the request doesn't ask for JSON API" do
+    pending
     get apiurl 
     expect(last_response.status).to eq 406
   end
 
   it "should accept a request that asks for JSON API" do
+    pending
     get apiurl, {}, { "HTTP_ACCEPT" => "application/vnd.api+json" }
     expect(last_response.status).to eq 200
   end
