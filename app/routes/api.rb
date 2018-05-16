c77b84a06b3a706212629f6b0c4ca1fabc59b105
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-16 19:12:58 -0400

    Get rid of redis in the api.

--- app/routes/api.rb
+++ app/routes/api.rb
@@ -57,13 +57,15 @@ class App
     before do
       halt 406 unless request.preferred_type.entry == mime_type(:api_json)
       @data = parse_request_body
-      if request.request_method == "POST"
+      if request.request_method =~ /(POST|PATCH|DELETE)/
         if @data.nil? || @data.length == 0
           halt 400, { "error": "no_request_payload" }.to_json
-        elsif redis.get("#{@data[:username]}_access_token") != @data[:access_token]
-        # Get authentication for posts.
-        # I expect a user and an access_token.
-          halt 400, { error: "authentication_error" }.to_json
+        else
+          user = User.where(username: @data[:username]).first
+          halt 400, { error: "authentication_error" }.to_json if user.nil?
+          if [:api_key] != @data[:api_key]
+            halt 400, { error: "authentication_error" }.to_json if user.nil?
+          end
         end
       end
       content_type :api_json
