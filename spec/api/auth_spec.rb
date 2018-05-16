011d57d5041d8c26f9987d4dd6a91b57bb556034
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-16 14:01:25 -0400

    Add working tests that check the access_token

--- spec/api/auth_spec.rb
+++ spec/api/auth_spec.rb
@@ -3,71 +3,26 @@ require_relative "./shared.rb"
 describe "NYWalker API" do
   include_context "api"
 
-  # This should all be covered w/ getting a key or writing a GitHub thing later.
+  # We only care about user info when posting. Creating an API key will be done
+  # separately from authenticating.
 
-  # context "logging in" do
+  context "when POSTing" do
+    let(:data_json) { accept.merge "CONTENT_TYPE" => "application/vnd.api+json" }
+    let(:user) { create :user }
+    let(:redis) { Redis.new }
 
-  #   context "with no payload" do
-  #     it "fails" do
-  #       post apiurl + "/token", {}, accept
-  #       expect(last_response.status).to eq 400
-  #     end
+    it "should give an error when the access token is wrong" do
+      redis.mset "#{user.username}_access_token", SecureRandom.urlsafe_base64
+      post apiurl + "/", { username: user.username, access_token: "hi!" }.to_json, data_json
+      expect(JSON.parse(last_response.body)["error"]).to eq "authentication_error"
+    end
 
-  #     it "returns an error of 'no_request_payload'" do
-  #       post apiurl + "/token", {}, accept
-  #       expect(JSON.parse(last_response.body)["error"]).to eq "no_request_payload"
-  #     end
-  #   end
+    it "should 200 when the access token is correct" do
+      redis.mset "#{user.username}_access_token", "hi!"
+      post apiurl + "/", { username: user.username, access_token: "hi!" }.to_json, data_json
+      expect(last_response.status).to eq 200
+    end
 
-  #   context "with a payload" do
+  end
 
-  #     let(:data) { '{"grant_type":"password", "username":"beetlejuice", "password":"beetlejuice"}' }
-  #     let(:data_json) { accept.merge "CONTENT_TYPE" => "application/vnd.api+json" }
-
-  #     it "fails when the data is not JSONAPI" do
-  #       post apiurl + "/token", data, accept
-  #       expect(last_response.status).to eq 415
-  #     end
-
-  #     it "sends a useful error on authentication issue" do
-  #       post apiurl + "/token", data, data_json
-  #       expect(JSON.parse(last_response.body)["error"]).to eq "invalid_grant"
-  #     end
-
-  #     it "authenticates and returns an access token" do
-  #       user = create(:user)
-  #       allow(user).to receive(:authenticate).and_return(user)
-  #       post apiurl + "/token", data, data_json
-  #       expect(JSON.parse(last_response.body)["access_token"]).not_to be_empty
-  #     end
-
-  #     it "sets an access token " do
-  #       redis = Redis.new
-  #       post apiurl + "/token", data, data_json
-  #       expect(JSON.parse(last_response.body)["access_token"]).to eq redis.get("beetlejuice_access_token")
-
-  #     end
-  #   end
-  # end
-
-  # context "logging out" do
-  #   let(:user) { create(:user) }
-  #   let(:data) { { account_id: user.id, access_token: "secret!" }.to_json }
-  #   let(:data_json) { accept.merge "CONTENT_TYPE" => "application/vnd.api+json" }
-  #   let(:redis) { Redis.new }
-
-  #   before do
-  #     redis.set "#{user.username}_access_token", "secret!"
-  #   end
-
-  #   it "sends a request to /revoke" do
-  #     post apiurl + "/revoke", {}, accept
-  #     expect(last_response.status).to eq 200
-  #   end
-
-  #   it "destroys the access token" do
-  #     post apiurl + "/revoke", data, data_json
-  #     expect(redis.get "#{user.username}-access-token").to be_nil
-  #   end
-  # end
 end
