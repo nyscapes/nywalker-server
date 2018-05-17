d91811fc20dbfafdf2b3698b5dfaf54bf2f671ad
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-17 13:57:23 -0400

    Move api model building to a helper method.

--- app/routes/api/all.rb
+++ app/routes/api/all.rb
@@ -1,23 +1,31 @@
 class App
   namespace '/api/v1' do
 
+    before do
+      @model = model unless request.path_info == "/api/v1/"
+    end
+
     %w(books places users instances nicknames).each do |route|
 
+      helpers do
+        def model
+          @model ||= request.path_info.gsub(/^\/api\/v1\/(\w*)(\/*.*$)/, '\1' ).singularize.classify.constantize
+        end
+      end
+
       get "/#{route}" do
-        model = route.singularize.classify.constantize
         #if params[:q] # do a query
         if params[:data_page] && params[:page_size]
-          build_total_pages(model, params[:page_size])
-          items = paginator(model.all, params)
+          build_total_pages(@model, params[:page_size])
+          items = paginator(@model.all, params)
           serialize_models(items).to_json
         else
-          serialize_models(model.all).to_json
+          serialize_models(@model.all).to_json
         end
       end
 
       get "/#{route}/:id" do
-        model = route.singularize.classify.constantize
-        item = model[params[:id].to_i]
+        item = @model[params[:id].to_i]
         not_found if item.nil?
         serialize_model(item).to_json
 
