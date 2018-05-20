5dedbe0f406cbde15b13e1254ddd6ef8457c12a2
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-20 03:17:34 -0400

    Add include data for certain relationships.

--- app/api.rb
+++ app/api.rb
@@ -6,7 +6,7 @@
 class BaseSerializer
   include JSONAPI::Serializer
 
-  # Provide the namespace
+  # Add the namespace prefix to the already existing #self_link
   def self_link
     "/api/v1#{super}"
   end
@@ -16,7 +16,7 @@ end
 class BookSerializer < BaseSerializer
   attributes :slug, :title, :author, :isbn, :year, :url, :cover, :added_on, :modified_on, :total_pages, :instances_per_page, :instance_count
 
-  has_many :instances
+  has_many :instances, include_data: true
   has_many :users
 
 end
@@ -25,8 +25,8 @@ end
 class PlaceSerializer < BaseSerializer
   attributes :name, :slug, :confidence, :source, :geonameid, :what3word, :bounding_box_string, :note, :lat, :lon, :added_on, :flagged, :instance_count, :nickname_sentence
 
-  has_many :instances
-  has_many :nicknames
+  has_many :instances, include_data: true
+  has_many :nicknames 
   has_one :user
 
 end
@@ -35,16 +35,16 @@ end
 class InstanceSerializer < BaseSerializer
   attributes :page, :sequence, :text, :note, :special, :added_on, :modified_on, :flagged, :lat, :lon, :place_name
 
-  has_one :place
+  has_one :place, include_data: true
   has_one :user
-  has_one :book
+  has_one :book, include_data: true
 end
 
 # This class serializes the Nickname model
 class NicknameSerializer < BaseSerializer
   attributes :name, :instance_count, :list_string #, :instance_count_query
 
-  has_one :place
+  has_one :place, include_data: true
 end
 
 # This class serializes the User model
