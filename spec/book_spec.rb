c4fdc16b0e558dd1aa604d6c0f448a1334875442
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-05-28 04:44:22 -0400

    These tests are a bit too easy.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -10,16 +10,34 @@ describe Book do
   end
 
   context "Methods" do
-    describe "#last_instance" do
-      it "returns the last instance"
+    let(:second_book) { create :second_book }
+    let(:place) { create :place }
+    let(:second_place) { create :place }
+    let(:no_place) { create :no_place }
+
+    before(:each) do
+      create :instance, book: book, place: place, text: "Istanbul"
+      create :instance, book: book, place: place, text: "Constantinople"
+      create :instance, book: book, place: place, text: "Istanbul"
+      create :instance, book: second_book, place: place, text: "Istanbul"
+      create :instance, book: book, place: second_place
+      create :instance, book: book, place: no_place
+      # create_list :instance, 15, book: book
     end
-    describe "#user_sentences" do
-      it "returns a list of users who contributed to the book."
-    end
+
     describe "#all_places" do
-      it "returns an array of pretend places"
-      it "that include the instance_count_by_name array"
-      it "includes only places from the book"
+      it "returns an array of places" do
+        # place, second_place, and no_place
+        expect(book.all_places.length).to eq 3 
+      end
+
+      it "that include the instance_count_by_name array" do
+        expect(book.all_places.map{|p| p[:instance_count_by_name].class}).to eq  [Array, Array, Array]
+      end
+      it "includes only places from the book" do
+        third_place = create :place
+        expect(book.all_places.map{|p| p[:id]}).to_not include third_place.id
+      end
     end
     describe "#mappable_places" do
       it "only includes places with a confidence not 0"
@@ -30,6 +48,12 @@ describe Book do
   end
 
   context "API Methods" do
+    describe "#last_instance" do
+      it "returns the last instance"
+    end
+    describe "#user_sentences" do
+      it "returns a list of users who contributed to the book."
+    end
     describe "#name"
     describe "#total_pages"
     describe "#instances_per_page"
