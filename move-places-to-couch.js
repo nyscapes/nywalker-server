29a361c47765705d2ab861f50035496578359847
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-10-19 14:19:51 -0400

    Get one document written to couch db.

--- move-places-to-couch.js
+++ move-places-to-couch.js
@@ -40,12 +40,31 @@ function cleanAssociations(data) {
     place.nicknames = JSON.parse(place.nicknames);
     return place;
   });
-  fs.writeFile(
-    path.join("data", "places-clean.json"),
-    JSON.stringify(cleanData, null, 2),
-    err => {
-      if (err) throw err;
-      process.stdout.write("places-clean.json written.\n");
-    }
-  );
+  insertData(data);
+  // fs.writeFile(
+  //   path.join("data", "places-clean.json"),
+  //   JSON.stringify(cleanData, null, 2),
+  //   err => {
+  //     if (err) throw err;
+  //     process.stdout.write("places-clean.json written.\n");
+  //   }
+  // );
+}
+
+function insertData(data) {
+  const place = data[0];
+  // data.map(place => {
+    couch.uniqid().then(ids => {
+      const id = ids[0];
+      couch.insert(dbName, Object.assign({_id: id}, place)).then(
+        // ({data, headers, status}) => {
+        ({status}) => {
+          process.stdout.write(`Status: ${status}\n`);
+        },
+        err => {
+          process.stdout.write(`Error: ${err}\n`);
+        }
+      );
+    });
+  // });
 }
