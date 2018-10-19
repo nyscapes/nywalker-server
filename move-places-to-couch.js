02177f12a7d9fc1fb3190478be7b69f41e693074
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-10-19 13:38:03 -0400

    Futz about with linters because that's the most important thing.

--- move-places-to-couch.js
+++ move-places-to-couch.js
@@ -1,35 +1,48 @@
-const NodeCouchDb = require("node-couchdb");
 const process = require("process");
 const fs = require("fs");
 const path = require("path");
+const NodeCouchDb = require("node-couchdb");
 
 const couch = new NodeCouchDb();
 const dbName = "text-walker";
-couch.createDatabase(dbName).then(() => {
-  readPlacesFile();
-}, err => {
-  process.stdout.write(err + "\n");
-});
 
-function readPlacesFile(){
+couch.dropDatabase(dbName).then(
+  () => createDB(),
+  err => {
+    process.stdout.write(err + "\n");
+    createDB();
+  }
+);
+
+function createDB() {
+  couch.createDatabase(dbName).then(
+    () => {
+      process.stdout.write(`db ${dbName} created\n`);
+      readPlacesFile();
+    },
+    err => {
+      process.stdout.write(err + "\n");
+    }
+  );
+}
+
+function readPlacesFile() {
   fs.readFile(path.join("data", "places.json"), (err, data) => {
     if (err) throw err;
     cleanAssociations(JSON.parse(data));
   });
 }
 
-// place.nicknames is a doubly encoded JSON string, so it needs separate parsing.
+// Place.nicknames is a doubly encoded JSON string, so it needs separate parsing.
 // and then write the json file
-function cleanAssociations(data){
-  fs.writeFile(path.join("data", "places-clean.json"),
-    JSON.stringify(
-      data.map(place => {
-        place.nicknames = JSON.parse(place.nicknames);
-        return place;
-      }),
-      null,
-      2
-    ),
+function cleanAssociations(data) {
+  const cleanData = data.map(place => {
+    place.nicknames = JSON.parse(place.nicknames);
+    return place;
+  });
+  fs.writeFile(
+    path.join("data", "places-clean.json"),
+    JSON.stringify(cleanData, null, 2),
     err => {
       if (err) throw err;
       process.stdout.write("places-clean.json written.\n");
